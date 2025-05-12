4b88aae26bd5f5162fa820c842cc1dba20ee4d20
Ahmet Şafak Yıldırım <114856193+ahm3texe@users.noreply.github.com>
2025-05-12 16:28:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.write("Suç detaylarını girin, ciddi mi (Part 1) yoksa hafif mi (Part 2) ol
 # Kullanıcıdan input alma
 time_occ = st.number_input("Suç Saati (0-1 arası, normalize; 0 = 00:00, 0.5 = 12:00, 1=23:59)", min_value=0.0, max_value=1.0, value=0.5)
 vict_age = st.number_input("Mağdur Yaşı (0-1 arası, normalize; 0 = 1, 0.5 = 55, 1=110)", min_value=0.0, max_value=1.0, value=0.5)
-lat = st.number_input("Enlem (0-1 arası, normalize)", min_value=0.0, max_value=1.0, value=0.5)
+lat = st.number_input("Enlem (0–1 arası, normalize; 0 = 33.5°, 0.5 = 33.9°, 1 = 34.3°)", min_value=0.0, max_value=1.0, value=0.5)
 crm_cd = st.number_input("Suç Kodu (0-500 arası, kodlanmış)", min_value=0, max_value=500, value=100)
 premis_cd = st.number_input("Olay Yeri Kodu (0-300 arası, kodlanmış)", min_value=0, max_value=300, value=100)
 
